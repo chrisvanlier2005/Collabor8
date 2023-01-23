0984782892912257267b9e99178bad3c08224f27
chrisvanlier2005 <87958@glr.nl>
2023-01-23 14:06:44 +0100

    Front-end Navbar style changes

---
+++ resources/js/Components/AppNavigation.vue
@@ -0,0 +1,50 @@
+<script setup>
+import {Link} from "@inertiajs/vue3";
+</script>
+<template>
+     <nav class="w-48 h-screen">
+        <ul class="flex w-full flex-col gap-3 pl-2 py-2 h-full inter">
+            <li class="w-full">
+                <Link class="w-full block rounded-sm p-2 py-1"
+                    :href="route('dashboard')" :class="{
+                    'active': $page.url === '/dashboard'
+                }">Home</Link>
+            </li>
+            <li class="w-full">
+                <Link class="w-full block rounded-sm p-2 py-1"
+                      :href="route('repositories.index')"
+                      :class="{
+                        'active': $page.url === '/repositories'
+                      }">Repositories</Link>
+            </li>
+            <li class="w-full"
+            >
+                <Link
+                    class="w-full block rounded-sm p-2 py-1"
+                    :href="route('users.index')"
+                    :class="{
+                        'active': $page.url === '/users'
+                    }">Users</Link>
+            </li>
+        </ul>
+    </nav>
+</template>
+<script>
+export default {
+    name: 'AppNavigation'
+}
+</script>
+<style scoped>
+.active {
+    color: #2e0046;
+    font-weight: 600;
+    background-color: rgba(82, 0, 123, 0.4);
+}
+ul li a {
+    transition: all 100ms;
+}
+ul li a:hover{
+    color: #2e0046;
+    background-color: rgba(82, 0, 123, 0.4);
+}
+</style>
--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -1,21 +1,10 @@
 <script setup>
 import {Link} from "@inertiajs/vue3";
+import AppNavigation from "@/Components/AppNavigation.vue";
 </script>
 <template>
     <section class="flex">
-        <nav class="w-48 h-screen">
-            <ul class="flex flex-col gap-3 pl-2 py-2 h-full">
-                <li>
-                    <Link :href="route('dashboard')">Home</Link>
-                </li>
-                <li>
-                    <Link :href="route('repositories.index')">Repositories</Link>
-                </li>
-                <li>
-                    <Link :href="route('users.index')">Users</Link>
-                </li>
-            </ul>
-        </nav>
+        <AppNavigation/>
         <main class="w-full">
             <div class="min-h-[4rem] border-b flex items-center flex-row inter header">
                 <slot name="header">
